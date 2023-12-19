96ae50f5d03c96bb8b6af0694ab16de4e4df52f6
mukesh kumar <mkmehta2041@gmail.com>
2023-12-19 13:48:13 +0530

    add infoCard to dashboard

---
+++ app/(root)/(dashboard)/(roots)/dashboard/_components/infoCard.jsx
@@ -0,0 +1,18 @@
+import { IconBadge } from "@/components/icon-bagde";
+import React from "react";
+
+const InfoCard = ({ variant, icon: Icon, numberofItems, label }) => {
+  return (
+    <div className="flex items-center gap-x-2 rounded-md border p-3">
+      <IconBadge variant={variant} icon={Icon} />
+      <div>
+        <p className="font-medium">{label}</p>
+        <p className="text-sm text-gray-500">
+          {numberofItems} {numberofItems === 1 ? "Course" : "Courses"}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default InfoCard;
--- app/(root)/(dashboard)/(roots)/dashboard/page.jsx
+++ app/(root)/(dashboard)/(roots)/dashboard/page.jsx
@@ -1,7 +1,34 @@
+import { redirect } from "next/navigation";
 import React from "react";
+import InfoCard from "./_components/infoCard";
+import { CheckCircle, Clock } from "lucide-react";
 
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  const userId = 12344;
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const coursesInProgress = 0;
+
+  return (
+    <div className="space-y-4 p-6">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 ">
+        <InfoCard
+          icon={Clock}
+          label="In Progress"
+          numberofItems={coursesInProgress}
+        />
+        <InfoCard
+          icon={CheckCircle}
+          label="Completed"
+          numberofItems={coursesInProgress}
+          variant="success"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
