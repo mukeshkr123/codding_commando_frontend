da917e4060eea745fd0947b8f0c4329b78f24b65
mukesh kumar <mkmehta2041@gmail.com>
2024-01-03 14:06:30 +0530

    fix nav avatar

--- components/dashboard/sidebar/nav-menu-avatar.jsx
+++ components/dashboard/sidebar/nav-menu-avatar.jsx
@@ -6,40 +6,54 @@ import {
   DropdownMenuItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "../../ui/dropdown-menu";
-import { RxAvatar } from "react-icons/rx";
+} from "@/components/ui/dropdown-menu";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { logoutAction } from "../../../GlobalRedux/slices/userSlice";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-const NavMenu = () => {
+export const UserAvatar = ({ imageUrl, name }) => {
   const dispatch = useDispatch();
   const router = useRouter();
+  const [isMounted, setIsMounted] = useState(false);
 
   const handleLogOut = () => {
     dispatch(logoutAction());
     router.push("/");
     toast.success("Logged out successfully");
   };
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <DropdownMenu className>
       <DropdownMenuTrigger className="border-none outline-none">
-        <RxAvatar className="h-8 w-8 border-none outline-none" />
+        <Avatar className="h-10 w-10 cursor-pointer hover:border-2">
+          <AvatarImage src={imageUrl} />
+          <AvatarFallback className="text-lg font-medium">
+            {name?.trim(" ").charAt(0).toUpperCase() || "P"}
+          </AvatarFallback>
+        </Avatar>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="px-2">
-        <DropdownMenuSeparator />
-        <DropdownMenuItem className="cursor-pointer text-base  font-medium">
+      <DropdownMenuContent className=" mr-2 flex flex-col gap-1 px-2">
+        <DropdownMenuItem className="cursor-pointer text-sm  font-medium">
           <Link href={`/dashboard`}>My Courses</Link>
         </DropdownMenuItem>
-        <DropdownMenuItem className="cursor-pointer text-base  font-medium">
+        <DropdownMenuItem className="cursor-pointer text-sm  font-medium">
           My Profile
         </DropdownMenuItem>
         <DropdownMenuItem
-          className="cursor-pointer text-base font-medium"
+          className="cursor-pointer text-sm font-medium"
           onClick={() => handleLogOut()}
         >
           LogOut
@@ -48,5 +62,3 @@ const NavMenu = () => {
     </DropdownMenu>
   );
 };
-
-export default NavMenu;
--- components/dashboard/sidebar/navbar-routes.jsx
+++ components/dashboard/sidebar/navbar-routes.jsx
@@ -4,45 +4,50 @@ import { LogOut } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
-import UserProfile from "./user-profile";
 import { useSelector } from "react-redux";
 import { Button } from "../../ui/button";
+import { UserAvatar } from "./nav-menu-avatar";
 
-export const NavbarRoutes = () => {
+const defaultAvatar = "default-avatar-url";
+
+const NavbarRoutes = () => {
   const userId = true;
   const pathname = usePathname();
   const { userAuth } = useSelector((state) => state?.user);
 
-  const isTeacher = (teacher) => {
-    return teacher;
-  };
-
-  const isTeacherPage = pathname.startsWith("/teacher");
+  const isTeacherPage = userId && pathname.startsWith("/teacher");
   const isCoursePage = pathname?.includes("/courses");
   const isSearchPage = pathname === "/search";
+
+  const linkButton = (href, children) => (
+    <Link href={href}>
+      <Button size="sm" variant="ghost">
+        {children}
+      </Button>
+    </Link>
+  );
+
   return (
     <>
-      {isSearchPage && <div>SeachInput</div>}
+      {isSearchPage && <div>SearchInput</div>}
 
-      <div className=" ml-auto flex gap-x-2">
-        {isTeacherPage || isCoursePage ? (
-          <Link href="/dashboard">
-            <Button size="sm" variant="ghost">
-              <LogOut className="mr-2 h-4 w-4" />
-              Exit
-            </Button>
-          </Link>
-        ) : isTeacher(userId) ? (
-          <Link href="/teacher/courses">
-            <Button variant="ghost" size="sm">
-              Teacher Mode
-            </Button>
-          </Link>
-        ) : null}
+      <div className="ml-auto flex gap-x-2">
+        {isTeacherPage || isCoursePage
+          ? linkButton(
+              "/dashboard",
+              <>
+                <LogOut className="mr-2 h-4 w-4" />
+                Exit
+              </>
+            )
+          : userId
+            ? linkButton("/teacher/courses", "Teacher Mode")
+            : null}
 
-        <div className="mx-2">
-          <UserProfile imageUrl={userAuth?.avatar} name={userAuth?.firstName} />
-        </div>
+        <UserAvatar
+          imageUrl={userAuth?.avatar ?? defaultAvatar}
+          name={userAuth?.firstName ?? "Guest"}
+        />
       </div>
     </>
   );
