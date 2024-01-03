0acced1f665ecc81652f4655f79f4bc0b02ee2a3
mukesh kumar <mkmehta2041@gmail.com>
2024-01-03 14:58:06 +0530

    add card

--- components/dashboard/search/course-list.jsx
+++ components/dashboard/search/course-list.jsx
@@ -1,5 +1,6 @@
 import apiClient from "lib/api-client";
 import React from "react";
+import { CourseCard } from "./cours-card";
 
 async function getAllCourses() {
   try {
@@ -14,8 +15,26 @@ async function getAllCourses() {
 
 export const CourseList = async () => {
   const courses = await getAllCourses();
-
-  console.log(courses);
-
-  return <div>CourseList</div>;
+  return (
+    <div>
+      <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 ">
+        {courses?.map((course) => (
+          <CourseCard
+            courseId={course?._id}
+            description={course?.description}
+            title={course?.title}
+            duration={course?.duration}
+            imageUrl={course?.imageUrl}
+            profilePicture={course?.profilePicture}
+            key={course?._id}
+          />
+        ))}
+      </div>
+      {courses?.length === 0 && (
+        <div className="mt-10 text-center text-sm text-muted-foreground">
+          No courses found
+        </div>
+      )}
+    </div>
+  );
 };
